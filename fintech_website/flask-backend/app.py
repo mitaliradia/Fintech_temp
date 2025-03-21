a2438b837b8983ed6ae840c74c7301acc0a36790
VedeshP <vedeshskp@gmail.com>
2025-03-21 13:21:44 +0530

    add db uri code

--- fintech_website/flask-backend/app.py
+++ fintech_website/flask-backend/app.py
@@ -1,8 +1,14 @@
 import os
 
 from flask import Flask, render_template, jsonify, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
