35a5f9d8ae040047536f5a6a85a9d8ed880ab851
mitaliradia <mitaliradia2947@gmail.com>
2025-03-22 13:45:07 +0530

    images added

--- fintech_website/frontend/postcss.config.cjs
+++ fintech_website/frontend/postcss.config.cjs
@@ -1,6 +1,7 @@
+// filepath: c:\Users\Mitali\Desktop\Fintech_temp\fintech_website\frontend\postcss.config.cjs
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
+  plugins: [
+    require('@tailwindcss/postcss7-compat'),
+    require('autoprefixer'),
+  ],
+};
