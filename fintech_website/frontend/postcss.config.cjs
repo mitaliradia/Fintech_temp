a6594d3791510e317b59f889fba88713f32d3796
Unaccomplished_Fun05 <150684514+Harshalsharma05@users.noreply.github.com>
2025-03-22 15:36:35 +0530

    psotfig file

--- fintech_website/frontend/postcss.config.cjs
+++ fintech_website/frontend/postcss.config.cjs
@@ -1,7 +1,7 @@
 // filepath: c:\Users\Mitali\Desktop\Fintech_temp\fintech_website\frontend\postcss.config.cjs
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss7-compat'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 };
